81b29ac637119fdd7bc09dee3a493d89385acef3
MichiKohlhaas <m.t.oconnell@hotmail.com>
2025-06-02 23:23:24 +0200

    Test of bg task

---
+++ Kohlhaas.DB/Program.cs
@@ -0,0 +1,20 @@
+using Kohlhaas.DB;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<MonitorLoop>();
+builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<IBackgroundTaskQueue>(_ =>
+{
+    if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
+    {
+        queueCapacity = 100;
+    }
+    return new DefaultBackgroundTaskQueue(queueCapacity);
+});
+
+var host = builder.Build();
+
+MonitorLoop monitorLoop = host.Services.GetRequiredService<MonitorLoop>();
+monitorLoop.StartMonitorLoop();
+
+host.Run();
