2aaf23d41f0f1b5f5d63d1c243a2a7f46d413837
MichiKohlhaas <m.t.oconnell@hotmail.com>
2025-12-18 19:17:38 +0100

    I forgot projects can be archived. Removed redundant ID since entity base has it and Project prefix to Code property (should be clear from context).

--- Kohlhaas.Domain/Entities/Project.cs
+++ Kohlhaas.Domain/Entities/Project.cs
@@ -4,14 +4,13 @@ namespace Kohlhaas.Domain.Entities;
 
 public class Project : EntityBase
 {
-    public Guid ProjectId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; }  = string.Empty;
-    public string ProjectCode { get; set; }  = string.Empty;
+    public string Code { get; set; }  = string.Empty;
     public VModelPhase CurrentPhase { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    
+    public bool IsArchived { get; set; }
     public virtual ICollection<ProjectMember> Members { get; set; } = [];
     
 }
